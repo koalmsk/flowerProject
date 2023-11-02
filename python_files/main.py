e6909004db46fc4040f0950ddc156a3273c6dcd5
koalmsk <denkosarev@gmail.com>
2023-11-02 23:26:25 +0300

    ignore_updates

--- python_files/main.py
+++ python_files/main.py
@@ -1,12 +1,13 @@
-from main_window import Main_window
-from PyQt5.QtWidgets import QApplication
-import sys
-from login_window import Login_window
-from database import db_operation
+# from main_window import Main_window
+# from PyQt5.QtWidgets import QApplication
+# import sys
+# from login_window import Login_window
+# from database import db_operation
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    example_window = Login_window()
-    db_operation.on_start_up()
-    example_window.show()
-    sys.exit(app.exec())
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     example_window = Login_window()
+#     db_operation.on_start_up()
+#     example_window.show()
+#     sys.exit(app.exec())
+
